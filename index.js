c212ad22943576db3058b8764a8366ab0a42ca6f
Omar <o.moreta@gmail.com>
2022-10-02 16:00:42 -0400

    added places controller

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
+const placesController = require("./controller/places");
+const PORT = process.env.PORT;
 const app = express();
 
-const PORT = process.env.PORT;
+//Middleware
+
+app.use("/places", placesController);
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
