9f1a25e12c68744fc88d37db4f17ba1ba9c95713
Omar <o.moreta@gmail.com>
2022-10-03 23:42:42 -0400

    added the edit form

--- controller/places.js
+++ controller/places.js
@@ -23,6 +23,18 @@ router.get("/:id", (req, res) => {
   }
 });
 
+//GET /places/:id/edit
+router.get("/:id/edit", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/edit", { place: places[id] });
+  }
+});
+
 //POST /places
 router.post("/", (req, res) => {
   console.log(req.body);
